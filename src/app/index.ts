d9357a939306388597cac6ea610c706351514bea
szabof-JS <szabof@jaystack.com>
2017-03-06 14:36:31 +0100

    Move system setup into system.ts instead of index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,20 +1,3 @@
-import { App, Server } from 'corpjs-express'
-import Config, { loaders, processors } from 'corpjs-config'
-import Endpoints from 'corpjs-endpoints'
-import Router from './Router'
-import System from 'corpjs-system'
-import Logger from 'corpjs-logger'
-import MongoDb from 'corpjs-mongodb'
+import system from './system'
 
-const inDevelopment = process.env.NODE_ENV === 'dev'
-process.on('unhandledRejection', err => console.error(err))
-
-export default new System({ exitOnError: !inDevelopment })
-    .add('config', new Config()
-        .add(config => loaders.require({ path: './config/default.js', mandatory: true })))
-    .add('logger', Logger()).dependsOn({ component: 'config', source: 'logger', as: 'config' })
-    .add('endpoints', Endpoints()).dependsOn({ component: 'config', source: 'endpoints', as: 'config' })
-    .add('mongodb', MongoDb()).dependsOn('endpoints', { component: 'config', source: 'mongodb', as: 'config' })
-    .add('app', App())
-    .add('router', Router()).dependsOn('app', 'endpoints', 'logger', 'mongodb')
-    .add('server', Server()).dependsOn('app', 'router', { component: 'config', source: 'server', as: 'config' })
+system.start()
---
+++ src/app/system.ts
@@ -0,0 +1,25 @@
+import { App, Server } from 'corpjs-express'
+import Config, { loaders, processors } from 'corpjs-config'
+import Endpoints from 'corpjs-endpoints'
+import Router from './Router'
+import System from 'corpjs-system'
+import Logger from 'corpjs-logger'
+import MongoDb from 'corpjs-mongodb'
+const {name} = require('../../package.json')
+
+const inDevelopment = process.env.NODE_ENV === 'dev'
+process.on('unhandledRejection', err => console.error(err))
+
+export default new System({ exitOnError: !inDevelopment })
+    .add('config', new Config()
+        .add(config => loaders.require({ path: './config/default.js', mandatory: true })))
+    .add('logger', Logger()).dependsOn({ component: 'config', source: 'logger', as: 'config' })
+    .add('endpoints', Endpoints()).dependsOn({ component: 'config', source: 'endpoints', as: 'config' })
+    .add('mongodb', MongoDb()).dependsOn('endpoints', { component: 'config', source: 'mongodb', as: 'config' })
+    .add('app', App())
+    .add('router', Router()).dependsOn('app', 'endpoints', 'logger', 'mongodb')
+    .add('server', Server()).dependsOn('app', 'router', { component: 'config', source: 'server', as: 'config' })
+    .on('componentStart', (componentName: string) => console.log(`Started component: ${componentName}`))
+    .on('componentStop', (componentName: string) => console.log(`Stopped component: ${componentName}`))
+    .on('start', resources => console.log(`Started service: ${name}`))
+    .on('stop', err => console.log(`Stopped service: ${name}`, err || ''))
