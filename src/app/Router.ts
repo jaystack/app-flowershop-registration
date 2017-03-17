8dd301cd2c90ecc3fcdd2f0cafb739768ec5b9a4
szabof-JS <szabof@jaystack.com>
2017-03-17 09:00:10 +0100

    Use cookie-parser

--- src/app/Router.ts
+++ src/app/Router.ts
@@ -1,8 +1,9 @@
 import { static as expressStatic } from 'express'
 import path = require('path')
-import bodyParser = require('body-parser');
-import express = require('express');
-import * as mongoose from 'mongoose';
+import bodyParser = require('body-parser')
+import cookieParser = require('cookie-parser')
+import express = require('express')
+import * as mongoose from 'mongoose'
 //import flash = require('connect-flash')
 import request = require('request')
 
@@ -13,12 +14,13 @@ export default function Router() {
     async start({ app, endpoints, logger, mongodb: db }) {
       const { getServiceAddress } = endpoints
       app.use(morganLogger('dev'))
-      app.use(bodyParser.json());
-      app.use(bodyParser.urlencoded({ extended: false }));
+      app.use(bodyParser.json())
+      app.use(bodyParser.urlencoded({ extended: false }))
+      app.use(cookieParser())
       //app.use(flash())
-      app.set('views', path.join('./views'));
-      app.set('view engine', 'hbs');
-      app.use(express.static('public'));
+      app.set('views', path.join('./views'))
+      app.set('view engine', 'hbs')
+      app.use(express.static('public'))
 
       const user = db.collection('users')
 
@@ -30,22 +32,36 @@ export default function Router() {
         }
       }
 
+      const getFlowersById = ((ids: Array<string>, clb: Function) => {
+        let p = ids.map(flowerId => new Promise((resolv, reject) => {
+          request.get({ url: `http://${endpoints.getServiceAddress('localhost:3003')}/data/flower(${flowerId})`, timeout: 4000 },
+            (err, catRes, flower) => {
+              //console.log(`# flower: ${JSON.stringify(flower)}`)
+              if (err) {console.log(err); return reject(err)}
+              resolv(JSON.parse(flower))
+            })
+        }))
+        Promise.all(p)
+          .then((flowers) => { clb(null, flowers) })
+          .catch((err) => { clb(err) })
+      })
+
       const router = express.Router()
+
+      router.use((req, res, next) => {
+        let cart
+        if (!!req.cookies && !!req.cookies["fs_cart"]) cart = (<any>req).cookies["fs_cart"]
+        if (!cart) cart = { created: new Date(), items: [] }
+        req['cart'] = cart
+        console.log('#cart:')
+        console.log(cart)
+        next()
+      })
 
       router.post('^(/|/registration)$', (req, res, next) => {
         console.log("# POST /registration/registration")
         const userObj = getUserObj(req.body);
         console.log(userObj)
-        // user.insertOne(userObj)
-        //   .then(result => {
-        //     console.log(`then: Successful registration!`)
-        //     res.redirect('/registration/success')
-        //   })
-        //   .catch(err => {
-        //     console.log(`catch: An error occured during registration!`)
-        //     console.log(err)
-        //     res.redirect('/registration/error')
-        //   })
         request.post(
           {
             url: `http://${endpoints.getServiceAddress('localhost:3003')}/data/user`,
@@ -60,21 +76,42 @@ export default function Router() {
             console.log(err)
             console.log(userRes.statusCode)
             console.log(user)
-            if (err) {
-              //return res.redirect('/registration/error') //res.sendStatus(500)
-              return res.render('error', { message: 'An error occured during registration!', error: err })
-            }
-            //if (userRes.statusCode !== 201) res.sendStatus(userRes.statusCode)
-            if (userRes.statusCode !== 201) res.status(userRes.statusCode)
-            //res.redirect('/registration/success')
-            res.render('error', { message: "Successful registration!" })
+            getFlowersById(req['cart'].items, (err, flowers) => {
+              console.log(`flowers: ${JSON.stringify(flowers)}`)
+              //if (err) { console.log(err); return res.sendStatus(500) }
+              let data = (!err || flowers.length > 0)
+                ? {
+                  cartValue: (flowers.reduce((a, b) => a + (b ? b.Price : 0), 0)).toFixed(2),
+                  cartItems: flowers,
+                }
+                : { cartValue: 0, cartItems:[] }
+              //res.render('error', { ...data, message: 'An error occured during registration!', error: err })
+              if (err) {
+                //return res.redirect('/registration/error') //res.sendStatus(500)
+                return res.render('error', { ...data, message: 'An error occured during registration!', error: err })
+              }
+              //if (userRes.statusCode !== 201) res.sendStatus(userRes.statusCode)
+              if (userRes.statusCode !== 201) res.status(userRes.statusCode)
+              //res.redirect('/registration/success')
+              res.render('error', { ...data, message: "Successful registration!" })
+            })
           })
       })
 
       router.get('^(/|/registration)$', (req, res, next) => {
         console.log("# GET /registration/registration")
         //res.sendFile(path.join(__dirname + '/../../views/registration.html'));
-        res.render('registration')
+        getFlowersById(req['cart'].items, (err, flowers) => {
+          console.log(`flowers: ${JSON.stringify(flowers)}`)
+          //if (err) { console.log(err); return res.sendStatus(500) }
+          let data = (!err || flowers.length > 0)
+            ? {
+              cartValue: (flowers.reduce((a, b) => a + (b ? b.Price : 0), 0)).toFixed(2),
+              cartItems: flowers,
+            }
+            : { cartValue: 0, cartItems:[] }
+          res.render('registration', data)
+        })
       });
 
       router.get('/success', (req, res, next) => {
